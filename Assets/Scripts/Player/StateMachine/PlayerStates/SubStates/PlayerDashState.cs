3e967c90b355408826a1c1ab42e0a963920e5a7d
CMAXMAPEN <acalde15@my.centennialcollege.ca>
2024-10-03 17:38:08 -0400

    Dash added
    Dash was made and implemented

--- Assets/Scripts/Player/StateMachine/PlayerStates/SubStates/PlayerDashState.cs
+++ Assets/Scripts/Player/StateMachine/PlayerStates/SubStates/PlayerDashState.cs
@@ -4,9 +4,91 @@ using UnityEngine;
 
 public class PlayerDashState : PlayerAbilityState
 {
+    public bool CanDash{get;private set;}
+    private bool isHolding;
+    private bool dashInputStop;
+    private float lastDashTime;
+    private Vector2 dashDirection;
+    private Vector2 dashDirectionInput;
     public PlayerDashState(Player player, PlayerStateMachine stateMachine, PlayerData playerdata, string animBoolName) : base(player, stateMachine, playerdata, animBoolName)
     {
     }
 
+    public override void Enter()
+    {
+        base.Enter();
+        CanDash = false;
+        player.InputHandler.UseDashInput();
+        isHolding = true;
+        dashDirection = Vector2.right * player.FacingDirection;
 
+        Time.timeScale = playerData.holdTimeScale;
+        startTime = Time.unscaledTime;
+
+        player.DashDirectionIndicator.gameObject.SetActive(true);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        if(player.CurrentVelocity.y > 0)
+        {
+            player.SetVelocityY(player.CurrentVelocity.y * playerData.dashEndYMultiplier);
+        }
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if(!isExitingState)
+        {
+            if(isHolding)
+            {
+                dashDirectionInput = player.InputHandler.DashDirectionInput;
+                dashInputStop = player.InputHandler.DashInputStop;
+
+                if(dashDirectionInput != Vector2.zero)
+                {
+                    dashDirection = dashDirectionInput;
+                    dashDirection.Normalize();
+                } 
+
+                float angle = Vector2.SignedAngle(Vector2.right, dashDirection);
+                player.DashDirectionIndicator.rotation = Quaternion.Euler(0f,0f,angle-45f);
+
+                if(dashInputStop || Time.unscaledTime >= startTime + playerData.maxHoldTime)
+                {
+                    isHolding = false;
+                    Time.timeScale = 1f;
+                    startTime = Time.time;
+                    player.CheckIfShouldFlip(Mathf.RoundToInt(dashDirection.x));
+                    player.rb.drag = playerData.drag;
+                    player.SetVelocity(playerData.dashVelocity, dashDirection);
+                    player.DashDirectionIndicator.gameObject.SetActive(false);
+                }
+            }
+        else
+        {
+           player.SetVelocity(playerData.dashVelocity, dashDirection);
+
+           if(Time.time >= startTime + playerData.dashTime)
+           {
+            player.rb.drag = 0f;
+            isAbilityDone = true;
+            lastDashTime = Time.time;
+           }
+        }
+
+        }
+    }
+
+    public bool CheckIfCanDash()
+    {
+        return CanDash && Time.time >= lastDashTime + playerData.dashCooldown;
+    }
+
+    public void ResetCanDash() => CanDash =true;
+    
 }
--- Assets/Scripts/Player/StateMachine/playerData/PlayerData.cs
+++ Assets/Scripts/Player/StateMachine/playerData/PlayerData.cs
@@ -38,6 +38,16 @@ public class PlayerData : ScriptableObject
     public float crouchColliderHeight = 0.8f;
     public float standColliderHeight = 1.6f;
 
+    [Header("Dash State")]
+    public float dashCooldown = 0.5f;
+    public float maxHoldTime = 1f;
+    public float holdTimeScale = 0.25f;
+    public float dashTime = 0.2f;
+    public float dashVelocity = 30f;
+    public float drag = 10f;
+    public float dashEndYMultiplier = 0.2f;
+    public float distBetweenAfterImages = 0.5f;
+
     [Header("Check Variables")]
     public float groundCheckRadius = 0.3f;
     public float wallCheckDistance = 0.5f;
