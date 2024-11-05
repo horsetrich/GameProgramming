73e11f76abea5f6b481bcbf46b38cf4d36e5afab
CMAXMAPEN <anthonys4.calderon@gmail.com>
2024-11-05 00:04:22 -0500

    Reapply "Added Scene"
    This reverts commit 9462f719ee03224bf1b6276feb085d402a8d0242.

---
+++ Assets/Scripts/Paralax/CameraFollow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    void LateUpdate ()
+    {
+        transform.position = target.position;
+    }
+}
