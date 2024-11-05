23383d7f80df051bc9a1ac86c2b17de2d7f71f11
CMAXMAPEN <anthonys4.calderon@gmail.com>
2024-11-05 00:07:41 -0500

    Revert "Reapply "Added Scene""
    This reverts commit 73e11f76abea5f6b481bcbf46b38cf4d36e5afab.

--- Assets/Scripts/General/CameraMovement.cs
+++ Assets/Scripts/General/CameraMovement.cs
@@ -7,16 +7,11 @@ public class CameraMovement : MonoBehaviour
     public Transform playerTransform;
     public Vector3 offset;
     public float smoothSpeed = 0.125f;
-    [SerializeField] float minX;
-    [SerializeField] float maxX;
-    [SerializeField] float minY;
-    [SerializeField] float maxY;
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
         Vector3 desiredPosition = playerTransform.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
-        transform.position = new Vector3(Mathf.Clamp(smoothedPosition.x,minX, maxX), Mathf.Clamp(smoothedPosition.y,minY, maxY), transform.position.y);
     }
 }
